e2e1bc0803b533a4da146a2529f69a2aa9f8c1ff
Agrcomp <dontreply@unit4.com>
2017-07-19 10:49:45 +0000

    Need to have this somewhere public, like github

---
+++ InstallVS2017.ps1
@@ -0,0 +1,16 @@
+﻿$bootstrapper = "https://aka.ms/vs/15/release/vs_buildtools.exe"
+$apppath="$PSScriptRoot\.app"
+$downloadedfile = "$apppath\vs_buildtools.exe"
+
+if(-not (Test-Path $downloadedfile)) {
+    New-Item -Force -ItemType directory -Path $apppath
+    Invoke-WebRequest $bootstrapper -OutFile $downloadedfile
+}
+
+
+&$downloadedfile update --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --passive --wait `    --add Microsoft.VisualStudio.Product.BuildTools `
+    --add Microsoft.VisualStudio.Workload.NativeDesktop `
+    --add Microsoft.VisualStudio.Workload.NetWeb `
+    --add Microsoft.VisualStudio.Workload.WebBuildTools
+
+
